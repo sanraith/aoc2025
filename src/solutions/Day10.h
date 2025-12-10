a7f33326bb6b4925a6ca74977405348433346ccf
Soma Zsják <sanraith@users.noreply.github.com>
2025-12-10 10:32:40 +0100

    Solved day 10 part 1.

--- src/solutions/Day10.h
+++ src/solutions/Day10.h
@@ -4,11 +4,13 @@
 #include <regex>
 #include <string>
 #include <algorithm>
+#include <bitset>
 #include <set>
 #include <map>
 #include <string>
 #include <ranges>
 #include <vector>
+#include <queue>
 using namespace aoc::util;
 
 namespace aoc::year2025 {
@@ -18,8 +20,12 @@ namespace aoc::year2025 {
         Day10() : Solution(2025, 10, "Factory") {}
 
         Result part1(const std::string_view input) override {
-            auto machines = parse(input);
-            return NotImplementedResult;
+            const auto machines = parse(input);
+            auto sum = 0ll;
+            for (const auto& machine : machines) {
+                sum += fewestPresses(machine.lights, machine.buttons);
+            }
+            return sum;
         }
 
         Result part2(const std::string_view input) override {
@@ -27,31 +33,78 @@ namespace aoc::year2025 {
         }
 
     private:
+        static constexpr int MAX_BITS = 32;
+        static constexpr char LIGHT_ON = '#';
+        static constexpr char LIGHT_OFF = '.';
+        using NumericBitSet = unsigned long;
+        using LightBitSet = std::bitset<MAX_BITS>;
+
         struct Machine {
-            const std::string lights{};
-            const std::vector<std::vector<int>> buttons{};
+            const std::string lightsStr{};
+            const LightBitSet lights{};
+            const std::vector<std::vector<int>> buttonVectors{};
+            const std::vector<LightBitSet> buttons{};
             const std::vector<int> joltages{};
         };
 
+        static int fewestPresses(const LightBitSet& target, const std::vector<LightBitSet>& buttons) {
+            if (target == LightBitSet{}) { return 0; }
+            std::set<NumericBitSet> visited{};
+            std::queue<std::pair<int, LightBitSet>> queue{};
+            queue.emplace(0, LightBitSet{});
+
+            while (!queue.empty()) {
+                const auto [count, lights] = queue.front();
+                queue.pop();
+                if (!visited.insert(lights.to_ulong()).second) { continue; }
+
+                const auto nextCount = count + 1;
+                for (const auto& button : buttons) {
+                    const auto next = lights ^ button;
+                    if (next == target) { return nextCount; }
+                    if (!visited.contains(next.to_ulong())) { queue.emplace(nextCount, next); }
+                }
+            }
+
+            return -1;
+        }
+
         static std::vector<Machine> parse(const std::string_view input) {
             return getLines(input) | std::views::transform([](auto& line) {
-                std::regex partsRe{R"(\[([\.#]+)\] (.+) \{(.+)\})"};
+                std::regex partsRegex{R"(\[([\.#]+)\] (.+) \{(.+)\})"};
                 std::smatch match{};
-                std::regex_search(line, match, partsRe);
+                std::regex_search(line, match, partsRegex);
+
                 auto lightsStr = match[1].str();
                 const auto buttonsStr = match[2].str();
                 const auto joltageStr = match[3].str();
+
+                LightBitSet bitLights{};
+                for (int i = 0; i < lightsStr.size(); i++) {
+                    const auto c = lightsStr[i];
+                    bitLights.set(i, c == LIGHT_ON);
+                }
 
                 const auto buttons = split(buttonsStr, " ") | std::views::transform([](const auto& buttonStr) {
                     return split(std::regex_replace(buttonStr, std::regex(R"([\(\)])"), ""), ",")
                            | std::views::transform([](const auto& numberStr) { return stoi(numberStr); })
                            | std::ranges::to<std::vector>();
                 }) | std::ranges::to<std::vector>();
-                const auto joltages = split(std::regex_replace(joltageStr, std::regex(R"([\{\}])"), ""), ",")
+                const auto bitButtons = buttons | std::views::transform([](const auto& button) {
+                    LightBitSet bitButton{};
+                    for (const int bitIndex : button) { bitButton.set(bitIndex); }
+                    return bitButton;
+                }) | std::ranges::to<std::vector>();
+
+                const auto joltages = split(joltageStr, ",")
                                       | std::views::transform([](const auto& numberStr) { return stoi(numberStr); })
                                       | std::ranges::to<std::vector>();
 
-                return Machine{std::move(lightsStr), std::move(buttons), std::move(joltages)};
+                return Machine{
+                    std::move(lightsStr), std::move(bitLights),
+                    std::move(buttons), std::move(bitButtons),
+                    std::move(joltages)
+                };
             }) | std::ranges::to<std::vector>();
         }
     };
--- tests/solutions/Day10Test.cpp
+++ tests/solutions/Day10Test.cpp
@@ -6,34 +6,33 @@ using namespace aoc::year2025;
 
 /** Tests for https://adventofcode.com/2025/day/10 */
 
-TEST_CASE ("Day10 for example input 1", "[example]") {
+TEST_CASE("Day10 for example input 1", "[example]") {
     const auto exampleInput1 = R"([.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7})";
 
     Day10 day{};
     testPart1(day, exampleInput1, 2);
-    testPart2(day, exampleInput1, "not_implemented");
+    testPart2(day, exampleInput1, 10);
 }
 
-TEST_CASE ("Day10 for example input 2", "[example]") {
+TEST_CASE("Day10 for example input 2", "[example]") {
     const auto exampleInput2 = R"([...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2})";
 
     Day10 day{};
     testPart1(day, exampleInput2, 3);
-    testPart2(day, exampleInput2, "not_implemented");
+    testPart2(day, exampleInput2, 12);
 }
 
-TEST_CASE ("Day10 for example input 3", "[example]") {
+TEST_CASE("Day10 for example input 3", "[example]") {
     const auto exampleInput3 = R"([.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5})";
 
     Day10 day{};
     testPart1(day, exampleInput3, 2);
-    testPart2(day, exampleInput3, "not_implemented");
+    testPart2(day, exampleInput3, 11);
 }
 
-TEST_CASE ("Day10 for puzzle input", "[puzzle]") {
+TEST_CASE("Day10 for puzzle input", "[puzzle]") {
     Day10 day{};
     const std::string input = loadPuzzleInput(day);
-    // testPart1(day, input, "not_implemented");
+    testPart1(day, input, 473);
     // testPart2(day, input, "not_implemented");
 }
-
